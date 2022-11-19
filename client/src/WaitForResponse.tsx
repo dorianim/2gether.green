29f7794b7ab78f2655b80f07fc8069c780f90c89
16flotho <tom.florian@hotmail.com>
2022-11-19 13:51:10 +0100

    feat; Project Details

--- client/src/WaitForResponse.tsx
+++ client/src/WaitForResponse.tsx
@@ -1,14 +1,39 @@
 import { Button } from "@mui/material";
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+
+interface ProjectDetails {
+  id: string;
+  status: string;
+  construction_time: number;
+  cost_per_month: number;
+  payoff_time: number;
+  revenue_per_month: number;
+  total_cost: number;
+}
+
+
 export default function WaitForResponse() {
   const { projectId } = useParams();
+  const [projectDetails, setProjectDetails] = React.useState<ProjectDetails>();
+
+  React.useEffect(() => {
+    handleRefresh();
+  }, []);
 
   const handleRefresh = async () => {
-    //TODO
+    fetch("http://localhost:8000/api/v1/project/" + projectId).then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+    }).then((data) => {
+      setProjectDetails(data);
+    });
   };
+
   return (
-    <div>
+    projectDetails?.status === "WaitingForApproval" ? 
+    (<div>
       <h2>Success! We will check your request. </h2>
       <h3>
         This will may take a while. Please return to this page in a few days.
@@ -18,6 +43,10 @@ export default function WaitForResponse() {
           Refresh
         </Button>
       </div>
-    </div>
+    </div>) : (
+      <div>
+        <h2>Your project has been approved!</h2>
+      </div>
+    )
   );
 }
