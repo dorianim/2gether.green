607db7bddf270f6e3baaffa182a869191b482d6f
Dorian Zedler <dorian@itsblue.de>
2022-11-19 11:52:36 +0100

    Chore: cleanup main

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,45 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Home from './Home'
-import '@fontsource/public-sans';
-import './main.css'
-import Projects from './Projects';
-import Organzier from './Organizer';
-import WaitForResponse from './WaitForResponse';
-import { CssVarsProvider } from '@mui/joy/styles';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "./Home";
+import "@fontsource/public-sans";
+import "./main.css";
+import Projects from "./Projects";
+import Organzier from "./Organizer";
+import WaitForResponse from "./WaitForResponse";
+import { CssVarsProvider } from "@mui/joy/styles";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Developer from './Developer';
+import Developer from "./Developer";
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <div className="root-div">
     <React.StrictMode>
-      <Router>
-      <Routes>
-    
-            <Route path="/projects" element={  <CssVarsProvider><Projects /> </CssVarsProvider>} />
-           
-          </Routes>
+      <CssVarsProvider>
+        <Router>
           <Routes>
-    
-            <Route path="/organize" element={  <CssVarsProvider><Organzier /> </CssVarsProvider>} />
-           
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/organize" element={<Organzier />} />
+            <Route path="/developer" element={<Developer />} />
+            <Route path="/submitted/waiting" element={<WaitForResponse />} />
+            <Route path="/" element={<Home />} />
           </Routes>
-          <Routes>
-    
-            <Route path="/developer" element={  <CssVarsProvider><Developer /> </CssVarsProvider>} />
-           
-          </Routes>
-          <Routes>
-    
-    <Route path="/submitted/waiting" element={  <CssVarsProvider><WaitForResponse /> </CssVarsProvider>} />
-   
-  </Routes>
-          
-          <Routes>
-            <Route path="/" element={<CssVarsProvider><Home /> </CssVarsProvider>} />
-          </Routes>
-          
-      </Router>
+        </Router>
+      </CssVarsProvider>
     </React.StrictMode>
   </div>
-);  
+);
