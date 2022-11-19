39670c31936de2e5e1e950794786d8d276fd4cbb
16flotho <tom.florian@hotmail.com>
2022-11-19 11:07:11 +0100

    feat: implement chart

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -3,6 +3,7 @@ import Button from '@mui/joy/Button';
 import './Home.css';
 import background from "../images/background.jpg";
 import { CssVarsProvider } from '@mui/joy/styles';
+import { Chart } from './Chart';
 function App() {
   const [count, setCount] = useState(0)
 
@@ -12,6 +13,7 @@ function App() {
       <h1>2getherGreen</h1>
       <CssVarsProvider>
       <Button variant="solid">Start</Button>
+      <Chart></Chart>
     </CssVarsProvider>
     </div>
   )
