08a63c9653295e03d5464072b118fdc8de722b96
Dorian Zedler <dorian@itsblue.de>
2022-11-19 13:07:38 +0100

    Feat: use hash router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,7 +6,12 @@ import "./main.css";
 import Projects from "./Projects";
 import Organzier from "./Organizer";
 import WaitForResponse from "./WaitForResponse";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  HashRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
 import Developer from "./Developer";
 import PageContainer from "./PageContainer";
 import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
@@ -17,8 +22,8 @@ export const darkTheme = createTheme({
   palette: {
     mode: "light",
     primary: {
-      main: '#90EE90',
-    }
+      main: "#90EE90",
+    },
   },
 });
 
@@ -26,19 +31,19 @@ root.render(
   <div className="root-div">
     <React.StrictMode>
       <PageContainer>
-      <ThemeProvider theme={darkTheme}>
-        <CssBaseline />
-        <Router>
-          <Routes>
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/organize" element={<Organzier />} />
-            <Route path="/developer" element={<Developer />} />
-            <Route path="/project/:projectId" element={<WaitForResponse />} />
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </Router>
-      </ThemeProvider>
-    </PageContainer>
-  </React.StrictMode>
-  </div >
+        <ThemeProvider theme={darkTheme}>
+          <CssBaseline />
+          <HashRouter>
+            <Routes>
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/organize" element={<Organzier />} />
+              <Route path="/developer" element={<Developer />} />
+              <Route path="/project/:projectId" element={<WaitForResponse />} />
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </HashRouter>
+        </ThemeProvider>
+      </PageContainer>
+    </React.StrictMode>
+  </div>
 );
