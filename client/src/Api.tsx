c4613dd91df363ebff07982e6538580a00f87ce0
Dorian Zedler <dorian@itsblue.de>
2022-11-20 09:29:55 +0100

    Chore: set fixed base url

--- client/src/Api.tsx
+++ client/src/Api.tsx
@@ -1,4 +1,1 @@
-export const BASE_URL =
-  !process.env.NODE_ENV || process.env.NODE_ENV === "development"
-    ? "http://localhost:8000/api/v1"
-    : "/api/v1";
+export const BASE_URL = "/api/v1";
